b4854f2b7498e1babbf2fb8d908eac0f2e748e43
Iulia Roshior <julieroshior2018@gmail.com>
2024-04-04 22:23:06 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,78 @@
+//Task 1
+let x = 20;
+let y = 58;
+let z = 42;
+
+let resultTask1 = x + y + z;
+
+console.log(x + y + z);
+console.log(resultTask1);
+
+// Task 2
+
+const secInMin = 60;
+const minInHour = 60;
+const hourInDay = 24;
+const dayInAge = 365;
+
+let myAge = 25;
+
+let resultTask2 = secInMin * minInHour * hourInDay * dayInAge * myAge;
+
+console.log(secInMin * minInHour * hourInDay * dayInAge * myAge);
+console.log(resultTask2);
+
+// Task 3
+
+let count = 42;
+let userName = "42";
+console.log(String(count),) "" + count, count.toString(2);
+console.log(+UserName, Number(userName), )
+
+
+// Task 4
+
+let a = 1;
+let b = 2;
+let c = "белых медведей";
+
+let resultTask4 = `${a}${b} ${c}`;
+
+console.log(resultTask4);
+
+
+// Task 5
+
+const access = "доступ";
+const seal = "морпех";
+const frost = "наледь";
+const reproach = "поперек";
+const chopped = "рубило";
+
+let resultTask5 = (access + seal + frost + reproach + chopped).length;
+
+console.log(access[5]);
+
+console.log(resultTask5);
+
+//Task 6
+
+let name = prompt ("Ведите ваше имя", "name");
+let age = prompt ("Ведите ваш возраст");
+
+let resultTask6 = ``
+
+let one =10;
+let two = 4;
+let count = 10;
+
+console.log(one, two);
+console.log(one <= count);
+
+let hasStatus = "status"
+let hasBackground == "back";
+let isOpen = "";
+
+let oneString = "10";
+
+console.log("hasStatus: ", )
